f8826ac20ee2f42de19f8b5645cb46d5aa6e196e
cshwk2020 <cshwk2020@gmail.com>
2025-02-12 00:36:47 +0800

    test commit

---
+++ src/PlantArray.js
@@ -0,0 +1,211 @@
+
+const plantsArray = [
+    {
+        category: "Air Purifying Plants",
+        plants: [
+            {
+                name: "Snake Plant",
+                image: "http://localhost:3333/snake-plant-5939187_1280.jpg",
+                description: "Produces oxygen at night, improving air quality.",
+                cost: "$15"
+            },
+            {
+                name: "Spider Plant",
+                image: "http://localhost:3333/chlorophytum-3530413_1280.jpg",
+                description: "Filters formaldehyde and xylene from the air.",
+                cost: "$12"
+            },
+            {
+                name: "Peace Lily",
+                image: "http://localhost:3333/peace-lilies-4269365_1280.jpg",
+                description: "Removes mold spores and purifies the air.",
+                cost: "$18"
+            },
+            {
+                name: "Boston Fern",
+                image: "http://localhost:3333/boston-fern-5114414_1280.jpg",
+                description: "Adds humidity to the air and removes toxins.",
+                cost: "$20"
+            },
+            {
+                name: "Rubber Plant",
+                image: "http://localhost:3333/flower-4850729_1280.jpg",
+                description: "Easy to care for and effective at removing toxins.",
+                cost: "$17"
+            },
+            {
+                name: "Aloe Vera",
+                image: "http://localhost:3333/leaf-3283175_1280.jpg",
+                description: "Purifies the air and has healing properties for skin.",
+                cost: "$14"
+            }
+        ]
+    },
+    {
+        category: "Aromatic Fragrant Plants",
+        plants: [
+            {
+                name: "Lavender",
+                image: "http://localhost:3333/dynamic1.jpg",
+                description: "Calming scent, used in aromatherapy.",
+                cost: "$20"
+            },
+            {
+                name: "Jasmine",
+                image: "http://localhost:3333/dynamic2.jpg",
+                description: "Sweet fragrance, promotes relaxation.",
+                cost: "$18"
+            },
+            {
+                name: "Rosemary",
+                image: "http://localhost:3333/rosemary-4541241_1280.jpg",
+                description: "Invigorating scent, often used in cooking.",
+                cost: "$15"
+            },
+            {
+                name: "Mint",
+                image: "http://localhost:3333/mint-1126282_1280.jpg",
+                description: "Refreshing aroma, used in teas and cooking.",
+                cost: "$12"
+            },
+            {
+                name: "Lemon Balm",
+                image: "http://localhost:3333/balm-4480134_1280.jpg",
+                description: "Citrusy scent, relieves stress and promotes sleep.",
+                cost: "$14"
+            },
+            {
+                name: "Hyacinth",
+                image: "http://localhost:3333/hyacinth-4110726_1280.jpg",
+                description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
+                cost: "$22"
+            }
+        ]
+    },
+    {
+        category: "Insect Repellent Plants",
+        plants: [
+            {
+                name: "oregano",
+                image: "http://localhost:3333/oregano-790702_1280.jpg",
+                description: "The oregano plants contains compounds that can deter certain insects.",
+                cost: "$10"
+            },
+            {
+                name: "Marigold",
+                image:"http://localhost:3333/marigold-7028063_1280.jpg",
+                description: "Natural insect repellent, also adds color to the garden.",
+                cost: "$8"
+            },
+            {
+                name: "Geraniums",
+                image: "http://localhost:3333/flowerpot-43270_1280.jpg",
+                description: "Known for their insect-repelling properties while adding a pleasant scent.",
+                cost: "$20"
+            },
+            {
+                name: "Basil",
+                image: "http://localhost:3333/tulsi-1539181_1280.jpg",
+                description: "Repels flies and mosquitoes, also used in cooking.",
+                cost: "$9"
+            },
+            {
+                name: "Lavender",
+                image: "http://localhost:3333/dynamic2.jpg",
+                description: "Calming scent, used in aromatherapy.",
+                cost: "$20"
+            },
+            {
+                name: "Catnip",
+                image: "http://localhost:3333/cat-829681_1280.jpg",
+                description: "Repels mosquitoes and attracts cats.",
+                cost: "$13"
+            }
+        ]
+    },
+    {
+        category: "Medicinal Plants",
+        plants: [
+            {
+                name: "Aloe Vera",
+                image: "http://localhost:3333/leaf-3283175_1280.jpg",
+                description: "Soothing gel used for skin ailments.",
+                cost: "$14"
+            },
+            {
+                name: "Echinacea",
+                image: "http://localhost:3333/echinacea-557477_1280.jpg",
+                description: "Boosts immune system, helps fight colds.",
+                cost: "$16"
+            },
+            {
+                name: "Peppermint",
+                image: "http://localhost:3333/peppermint-2496773_1280.jpg",
+                description: "Relieves digestive issues and headaches.",
+                cost: "$13"
+            },
+            {
+                name: "Lemon Balm",
+                image: "http://localhost:3333/balm-4480134_1280.jpg",
+                description: "Calms nerves and promotes relaxation.",
+                cost: "$14"
+            },
+            {
+                name: "Chamomile",
+                image: "http://localhost:3333/flowers-1606041_1280.jpg",
+                description: "Soothes anxiety and promotes sleep.",
+                cost: "$15"
+            },
+            {
+                name: "Calendula",
+                image: "http://localhost:3333/flowers-4340127_1280.jpg",
+                description: "Heals wounds and soothes skin irritations.",
+                cost: "$12"
+            }
+        ]
+    },
+    {
+        category: "Low Maintenance Plants",
+        plants: [
+            {
+                name: "ZZ Plant",
+                image: "http://localhost:3333/dynamic3.jpg",
+                description: "Thrives in low light and requires minimal watering.",
+                cost: "$25"
+            },
+            {
+                name: "Pothos",
+                image: "http://localhost:3333/plants-3816945_1280.jpg",
+                description: "Tolerates neglect and can grow in various conditions.",
+                cost: "$10"
+            },
+            {
+                name: "Snake Plant",
+                image: "http://localhost:3333/snake-plant-5939187_1280.jpg",
+                description: "Needs infrequent watering and is resilient to most pests.",
+                cost: "$15"
+            },
+            {
+                name: "Cast Iron Plant",
+                image: "http://localhost:3333/cast-iron-plant-2072008_1280.jpg",
+                description: "Hardy plant that tolerates low light and neglect.",
+                cost: "$20"
+            },
+            {
+                name: "Succulents",
+                image: "http://localhost:3333/cacti-1846147_1280.jpg",
+                description: "Drought-tolerant plants with unique shapes and colors.",
+                cost: "$18"
+            },
+            {
+                name: "Aglaonema",
+                image: "http://localhost:3333/aglaonema-482915_1280.jpg",
+                description: "Requires minimal care and adds color to indoor spaces.",
+                cost: "$22"
+            }
+        ]
+    }
+];
+
+
+export default plantsArray;
